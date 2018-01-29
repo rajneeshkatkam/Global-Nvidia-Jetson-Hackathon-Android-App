414dafa9774a3613827b8b6f53a56425e68090aa
Rajneesh Katkam <rajneesh.katkam@gmail.com>
2018-01-30 04:09:18 +0530

    Text to speech and speech to text basic modules are ready..jdFirebase database and authentication is also setup..

---
+++ nvidiaHack/app/src/main/java/com/raj/nvidiahack/Info.java
@@ -0,0 +1,30 @@
+package com.raj.nvidiahack;
+
+/**
+ * Created by RAJ on 29-01-2018.
+ */
+
+
+public class Info {
+
+    public String onDate,lateDate,onTime,lateTime;
+    public Integer lateAttendanceCount,onTimeAttendanceCount;
+
+    Info()
+    {
+
+    }
+
+    public Info(Integer lateAttendanceCount,String lateDate,String lateTime,Integer onTimeAttendanceCount,String onDate,String onTime)
+    {
+        this.lateAttendanceCount=lateAttendanceCount;
+        this.lateDate=lateDate;
+        this.lateTime=lateTime;
+
+        this.onTimeAttendanceCount=onTimeAttendanceCount;
+        this.onDate=onDate;
+        this.onTime=onTime;
+
+    }
+
+}
--- nvidiaHack/app/src/main/java/com/raj/nvidiahack/mainActivityAfterSuccessfulLogin.java
+++ nvidiaHack/app/src/main/java/com/raj/nvidiahack/mainActivityAfterSuccessfulLogin.java
@@ -1,13 +1,203 @@
 package com.raj.nvidiahack;
 
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.speech.RecognizerIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 public class mainActivityAfterSuccessfulLogin extends AppCompatActivity {
+    private static final String TAG = "afterLoginActivity";
+
+    private TextView txtSpeechInput;
+    private ImageButton btnSpeak;
+    private final int REQ_CODE_SPEECH_INPUT = 100;
+
+
+    DatabaseReference mDatabaseReference;
+    FirebaseAuth mAuth;
+    SharedPreferences file;
+    Boolean flag;
+    String uid;
+
+    Info attendance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_after_successful_login);
+
+        mAuth=FirebaseAuth.getInstance();
+        uid=mAuth.getCurrentUser().getUid();
+        mDatabaseReference=FirebaseDatabase.getInstance().getReference();
+
+
+        txtSpeechInput = (TextView) findViewById(R.id.txtSpeechInput);
+        btnSpeak = (ImageButton) findViewById(R.id.btnSpeak);
+
+
+
+        ///Storage of flag value after the app is killed or shutdown
+        file = getSharedPreferences("save", 0);
+        flag = Boolean.valueOf(file.getString("flag", "false"));
+        if (flag == false)////File permission check
+            checkFilePermissions();
+
+        SharedPreferences.Editor edit = file.edit();
+        edit.putString("flag", String.valueOf(flag));
+        edit.apply();
+
+
+        ////Real time database update and control
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                //fetchValuesFromDatabase(dataSnapshot);
+            }
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+
+        ///
+        btnSpeak.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                promptSpeechInput();
+            }
+        });
+
+
+
+
+
+
+
     }
+
+
+    /////Speech To Text Code Start
+
+    /**
+     * Showing google speech input dialog
+     * */
+    private void promptSpeechInput() {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        intent.putExtra(RecognizerIntent.EXTRA_PROMPT,
+                getString(R.string.speech_prompt));
+        try {
+            startActivityForResult(intent, REQ_CODE_SPEECH_INPUT);
+        } catch (ActivityNotFoundException a) {
+            Toast.makeText(getApplicationContext(),
+                    getString(R.string.speech_not_supported),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Receiving speech input
+     * */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQ_CODE_SPEECH_INPUT: {
+                if (resultCode == RESULT_OK && null != data) {
+
+                    ArrayList<String> result = data
+                            .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                    txtSpeechInput.setText(result.get(0));
+                }
+                break;
+            }
+
+        }
+    }
+
+    ////////Speech to Text Code End
+
+
+
+    ////DataBase Update and Fetch
+
+    public void fetchValuesFromDatabase(DataSnapshot dataSnapshot)
+    {
+        /// Irrigation Values
+        attendance =dataSnapshot.child(uid).child("attendance").getValue(Info.class);
+
+    }
+
+
+    void print(String s)
+    {
+        Toast.makeText(getApplicationContext(),s,Toast.LENGTH_SHORT).show();
+    }
+
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        if (mAuth.getCurrentUser() == null) {
+
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+
+        }
+        // updateUI(currentUser);
+    }
+
+
+    ////File Permission Check
+    private void checkFilePermissions() {
+
+
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP) {
+            int permissionCheck = mainActivityAfterSuccessfulLogin.this.checkSelfPermission("Manifest.permission.READ_EXTERNAL_STORAGE");
+            permissionCheck += mainActivityAfterSuccessfulLogin.this.checkSelfPermission("Manifest.permission.WRITE_EXTERNAL_STORAGE");
+            if (permissionCheck != 0) {
+                this.requestPermissions(new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE, android.Manifest.permission.READ_EXTERNAL_STORAGE}, 1001); //Any number
+            }
+            flag = true;
+        } else {
+            Log.d(TAG, "checkBTPermissions: No need to check permissions. SDK version < LOLLIPOP.");
+        }
+
+    }
+
+
+    ///textToSpeechActivity Button onClick for Navigating to TTS activity
+
+    public void textToSpeechActivity(View v)
+    {
+        startActivity(new Intent(getApplicationContext(),textToSpeechActivity.class));
+        finish();
+    }
+
+
+
 }
+
+
