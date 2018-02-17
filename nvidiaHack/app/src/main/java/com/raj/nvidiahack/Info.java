06c004b09c600ff08398a938471cccc257f21552
Rajneesh Katkam <rajneesh.katkam@gmail.com>
2018-02-17 22:50:47 +0530

    Added heat map count and the image count]

--- nvidiaHack/app/src/main/java/com/raj/nvidiahack/Info.java
+++ nvidiaHack/app/src/main/java/com/raj/nvidiahack/Info.java
@@ -7,24 +7,24 @@ package com.raj.nvidiahack;
 
 public class Info {
 
-    public String onDate,lateDate,onTime,lateTime;
-    public Integer lateAttendanceCount,onTimeAttendanceCount;
+    public String onDate, lateDate, onTime, lateTime;
+    public Integer lateAttendanceCount, onTimeAttendanceCount;
+    public Integer imageCount;
 
-    Info()
-    {
+    Info() {
+
+    }
+
+    public Info(Integer lateAttendanceCount, String lateDate, String lateTime, Integer onTimeAttendanceCount, String onDate, String onTime) {
+        this.lateAttendanceCount = lateAttendanceCount;
+        this.lateDate = lateDate;
+        this.lateTime = lateTime;
+
+        this.onTimeAttendanceCount = onTimeAttendanceCount;
+        this.onDate = onDate;
+        this.onTime = onTime;
 
     }
 
-    public Info(Integer lateAttendanceCount,String lateDate,String lateTime,Integer onTimeAttendanceCount,String onDate,String onTime)
-    {
-        this.lateAttendanceCount=lateAttendanceCount;
-        this.lateDate=lateDate;
-        this.lateTime=lateTime;
-
-        this.onTimeAttendanceCount=onTimeAttendanceCount;
-        this.onDate=onDate;
-        this.onTime=onTime;
-
-    }
 
 }
